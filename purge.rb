33c46b28be1681b086718d24a36b8596cf3054ed
beepony <beeponky@gmail.com>
2015-10-19 18:45:11 +0800

    modified the purge.rb

--- purge.rb
+++ purge.rb
@@ -2,31 +2,35 @@
 # coding:utf-8
 # purge in ruby
 
-bucket = demo-bucket
-password = demo-password
-uri-list = "http://"
+require 'rest-client'
+require 'json'
+===================================================
+bucket = 'bucket'
+operator = 'operator'
+password = 'password'
+purge_api = 'http://purge.upyun.com/purge/'
+purge_list = "http://bucket.b0.upaiyun.com/test.jpg"
+===================================================
 
-uri = 'http://purge.upyun.com/purge/'
-res = RestClient::Resource.new ("#{uri}", opts)
-
-opts = {
-
-	 :Authorization => #{auth},
-     :Date => #{gmdate},
-     :purge => #{url-list},
-}
+def md5(str)
+  Digest::MD5.hexdigest(str)
+end
 
 gmdate = Time.now.utc.strftime('%a, %d %b %Y %H:%M:%S GMT')
 
-sign = "#{uri-list}&#{bucket}&#{gmdate}&#{md5(password)}"
+sign = md5("#{uri_list}&#{bucket}&#{gmdate}&#{md5(password)}")
 
-auth = "UpYun #{bucket}:#{password}:#{md5(sign)}"
+auth = "UpYun #{bucket}:#{operator}:#{sign}"
 
-def md5(str)
-  Digest::MD5.hexdigest("str")
-end
+headers = {'Authorization' => auth,'Date' => gmdate}
 
+payload = {'purge' => uri_list}
 
-  
+#puts headers,payload
+
+res = RestClient.post uri,payload,headers
+
+puts res.headers,res.body
+#hds.each do |k,v| puts "#{k}:#{v}" end  
 
 
